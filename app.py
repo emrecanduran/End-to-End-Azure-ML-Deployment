ba65c6a0e36c3e88bfecb0162642b07b0999d013
emrecanduran <eemrecanduran@gmail.com>
2024-04-27 16:10:22 +0100

    Updated commit

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def predict():
     return render_template("home.html", prediction_text="The house price prediction is {}".format(output))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    app.run()
